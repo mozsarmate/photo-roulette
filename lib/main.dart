c75e5232489354b9b010b79734378c01a1ded0d9
mozsarmate <mozsarmatee@gmail.com>
2024-01-22 15:39:04 +0100

    imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FireStore ',
       theme: ThemeData(
         // This is the theme of your application.
         //
