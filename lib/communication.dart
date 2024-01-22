b234e003e81ff3444266abb9d73cfbcc67db3710
Németh Marcell <nemeth.marcell.nm@gmail.com>
2024-01-22 20:36:50 +0100

    fixing dataschemes and lobby generation

--- lib/communication.dart
+++ lib/communication.dart
@@ -1,10 +1,6 @@
-import 'dart:html';
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
 import 'firebase_options.dart';
 import 'dart:math';
@@ -14,11 +10,16 @@ class Comunicator{
   void _initRoom(FirebaseFirestore firestore){
     Random rng = Random();
     int code = rng.nextInt(100) + rng.nextInt(9)*1000; //generate a fix 4 digit num
-    Map<String, dynamic> players = {
-      'name1' : '',
-      'ready1' : '',
+    Map<String, dynamic> static = {
+      'images' : [],
+      'order' : [],
     };
-    firestore.collection(code.toString()).add(players);
+    Map<String, dynamic> state = {
+      'round' : 0,
+      'players' : [],
+    };
+    firestore.collection(code.toString()).doc('static').set(static);
+    firestore.collection(code.toString()).doc('state').set(state);
   }
 
   void _createSeq(){
