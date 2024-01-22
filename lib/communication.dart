8063cc94b48bf0eb0077bc80b72048565afb931a
Németh Marcell <nemeth.marcell.nm@gmail.com>
2024-01-22 22:16:34 +0100

    db fix

--- lib/communication.dart
+++ lib/communication.dart
@@ -10,16 +10,19 @@ class Comunicator{
   void _initRoom(FirebaseFirestore firestore){
     Random rng = Random();
     int code = rng.nextInt(100) + rng.nextInt(9)*1000; //generate a fix 4 digit num
+    String room = code.toString();
     Map<String, dynamic> static = {
       'images' : [],
       'order' : [],
+      'owner' : []
     };
     Map<String, dynamic> state = {
       'round' : 0,
       'players' : [],
     };
-    firestore.collection(code.toString()).doc('static').set(static);
-    firestore.collection(code.toString()).doc('state').set(state);
+    firestore.collection(room).doc("static").set(static).onError((e, _) => print("Error writing document: $e"));;
+    firestore.collection(room).doc("state").set(state).onError((e, _) => print("Error writing document: $e"));;
+
   }
 
   void _createSeq(){
