9a300bdb02eb28fc153cfb4a47b656b3c894dfa3
Szabó Benedek <benedek.szabo2003@gmail.com>
2024-01-23 11:37:56 +0100

    Navigation test page

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:photo_roulette/pages/leaderboard.dart';
+import 'package:flutter/services.dart';
+import 'package:photo_roulette/pages/home.dart';
+import 'package:photo_roulette/pages/resultsPanel.dart';
 import 'package:photo_roulette/themes/themes.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:provider/provider.dart';
 
-import 'models/communication.dart';
 import 'firebase_options.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -25,18 +24,22 @@ void main() async {
       child: MyApp(),
     ),
   );
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: "Photo Roulette",
         //home: const RoundScreen(gamePin: "123"),
-        home: LeaderBoardPage(),
-        theme: mainTheme);
+        home: HomePage(),
+        theme: mainTheme
+    );
   }
 }
