7a01b6292784018457b47bc99afed11f1f9ced4e
Miki <miki.riba70@gmail.com>
2024-01-23 03:33:54 +0100

    image picker majdnem

--- lib/pages/imagePickerPage.dart
+++ lib/pages/imagePickerPage.dart
@@ -1,65 +1,71 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 import '../components/assetThumbnail.dart';
 
-class ImagePickerPage extends StatelessWidget {
+class ImagePickerPage extends StatefulWidget {
   const ImagePickerPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    List<String> imagePaths = [
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png",
-      "assets/images/avatar_example.png"
-    ];
-    List<Image> images = [];
+  State<ImagePickerPage> createState() => _ImagePickerPageState();
+}
 
-    for (int i = 0; i < imagePaths.length; i++) {
-      // images.add(Image(
-      //     image: AssetThumbnail()),
-      //     width: 30,
-      //     height: 30
-      // ));
-    }
+class _ImagePickerPageState extends State<ImagePickerPage> {
+
+  List<AssetEntity> images = [];
+  final maxAgeOfImage = 500;
+  final numOfImagesWanted = 16;
+  var numOfImagesGot = 2;
+
+  @override
+  Widget build(BuildContext context) {
 
     return Scaffold(
         backgroundColor: Color(0xFF1A36B9),
         body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Center(
-                child: Container(
-                  width: 300,
-                  height: 300,
-                  color: Colors.amber,
-                  // child: GridView.builder(
-                  //   gridDelegate:
-                  //       const SliverGridDelegateWithFixedCrossAxisCount(
-                  //     crossAxisCount: 4,
-                  //     mainAxisSpacing: 2.0,
-                  //     crossAxisSpacing: 2.0,
-                  //   ),
-                  //   itemCount: images.length,
-                  //   itemBuilder: (_, index) {
-                  //     return AssetThumbnail(image: images[index]);
-                  //   },
-                  //)
-                ),
+              SizedBox(height: 20.0),
+              Expanded(
+                child: SizedBox(
+                    height: 200.0,
+                    child: GridView.builder(
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                        mainAxisSpacing: 2.0,
+                        crossAxisSpacing: 2.0,
+                      ),
+                      itemCount: images.length,
+                      itemBuilder: (_, index) {
+                        return AssetThumbnail(image: images[index]);
+                      },
+                    )),
               ),
+              ElevatedButton(onPressed: _pickRandomImage, child: Text("Pick random images"))
             ]));
   }
+
+  Future<void> _pickRandomImage() async {
+    var status = await PhotoManager.requestPermissionExtend();
+
+    if (status.hasAccess) {
+      // Permission is granted, proceed to access images
+      images = await PhotoManager.getAssetListRange(start: 0, end: maxAgeOfImage);
+      images.shuffle();
+      numOfImagesGot = min(images.length, numOfImagesWanted);
+      images = images.take(numOfImagesWanted).toList();
+      setState(() {});
+    } else {
+      print('Permission Denied');
+      // Permission is denied by the user
+      // Handle the case where the user denies permission
+      // You may want to display a message or guide the user to enable the permission manually
+    }
+  }
 }
+
+
+
